74e210b7cca712c21e93f10ff815884ed1d956bc
JBrown1342 <35350655+JBrown1342@users.noreply.github.com>
2018-03-25 20:08:36 -0700

    start

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    bodyParser = require('body-parser');
+
 var Pool = require('pg').Pool;
 var config = {
   host: 'localhost',
-  user: '',
-  password: '',
+  user: 'postgres',
+  password: 'Berklee123',
   database: 'show_database',
 }
 
+app.use(bodyParser.json());
 pool = new Pool(config);
 
 async function get_artists(){
   var response = await pool.query("SELECT * from artists");
   console.log(response.rows);
-  app.get('/', (req, res) => res.send(response.rows));
+  app.get('/', (req, res) => res.send(response.artist_name));
 };
 
 get_artists();
